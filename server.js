57aa52a6a9f5019bd3d31befeb8a12c5f154814e
DOBLEJ19962 <jmgonz103@gmail.com>
2025-09-17 00:55:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir solo archivos estáticos desde public
-app.use(express.static('public'));
+// Servir archivos estáticos desde public
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Forzar que cualquier ruta cargue index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+// Forzar que cualquier ruta cargue index.html (para SPA)
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
